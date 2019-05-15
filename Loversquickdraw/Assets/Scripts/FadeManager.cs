19e10efd779a69bdec1e2ce5b0385fc46f0eab00
kazuya <yuuki0523yata@gmail.com>
2019-05-15 15:42:21 +0900

    Fade関係の作成終了(実機テストは金曜)

--- Loversquickdraw/Assets/Scripts/FadeManager.cs
+++ Loversquickdraw/Assets/Scripts/FadeManager.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 
 public class FadeManager : MonoBehaviour
 {
+    private OVRInput.Button _button;
+    private int _buttonNum = 1;
     [SerializeField] private float speed;
     private float alfa;
     private float red, green, blue;
@@ -15,9 +17,26 @@ public class FadeManager : MonoBehaviour
         red = GetComponent<Image>().color.r;
         green = GetComponent<Image>().color.g;
         blue = GetComponent<Image>().color.b;
+        _button = InputManager.GetButton(_buttonNum);
+        this.gameObject.SetActive(false);
     }
     private void Update()
     {
+        CheckInput(_button);
+    }
+    private void CheckInput(OVRInput.Button button)
+    {
+        Fadeout();
+    }
+    private void Fadein()
+    {
+        GetComponent<Image>().color = new Color(red, green, blue, alfa);
+        alfa -= speed;
+        this.gameObject.SetActive(false);
+    }
+    private void Fadeout()
+    {
+        this.gameObject.SetActive(true);
         GetComponent<Image>().color = new Color(red, green, blue, alfa);
         alfa += speed;
     }
