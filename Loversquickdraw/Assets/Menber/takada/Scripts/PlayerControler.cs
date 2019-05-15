e277fae453b78735475e756f195a033ace139ff8
高田将志 <masapo.0315@icloud.com>
2019-05-16 00:20:51 +0900

    仮床と仮壁を配置

---
+++ Loversquickdraw/Assets/Menber/takada/Scripts/PlayerControler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour {
+
+    public float Speed = 10.0f;
+    public Rigidbody Player1Rb; //１Pのrigitbody
+    public Rigidbody Player2Rb; //２Pのrigitbody
+
+    // Use this for initialization
+    void Start () {
+        Player1Rb = GetComponent<Rigidbody>();
+        Player2Rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //スピードアップ
+    void SpeedUp()
+    {
+
+    }
+}
