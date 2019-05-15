4d3f3e502b7273ab213642a6922187d4e0d5f409
kazuya <yuuki0523yata@gmail.com>
2019-05-15 15:10:30 +0900

    FadeOutの作成

--- Loversquickdraw/Assets/Scripts/FadeManager.cs
+++ Loversquickdraw/Assets/Scripts/FadeManager.cs
@@ -1,8 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FadeManager : MonoBehaviour
 {
+    [SerializeField] private float speed;
+    private float alfa;
+    private float red, green, blue;
 
+    private void Start()
+    {
+        //Panelの色の取得
+        red = GetComponent<Image>().color.r;
+        green = GetComponent<Image>().color.g;
+        blue = GetComponent<Image>().color.b;
+    }
+    private void Update()
+    {
+        GetComponent<Image>().color = new Color(red, green, blue, alfa);
+        alfa += speed;
+    }
 }
