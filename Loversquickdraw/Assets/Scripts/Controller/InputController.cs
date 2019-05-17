e6f51551bd6a9aa0637fe007746d1135da2ac6da
kazuya <yuuki0523yata@gmail.com>
2019-05-17 19:59:50 +0900

    振ったことのスクリプト書き始め

--- Loversquickdraw/Assets/Scripts/Controller/InputController.cs
+++ Loversquickdraw/Assets/Scripts/Controller/InputController.cs
@@ -5,45 +5,61 @@ using UnityEngine;
 //岩崎
 public class InputController : MonoBehaviour
 {
-
-	void Update ()
+    [SerializeField] GameObject Lcube;
+    [SerializeField] GameObject Rcube;
+    int a = 0;
+    Vector3 L_defPos;
+    Vector3 R_defPos;
+    void Update ()
     {
-        if (OVRInput.GetDown(OVRInput.Touch.One))
+
+        L_defPos = Lcube.gameObject.transform.position;
+        R_defPos = Rcube.gameObject.transform.position;
+        if (L_defPos.y < -0.7f || L_defPos.y > 0f)
         {
-            Debug.Log("Aボタンを押した");
+            Debug.Log("aaa");
         }
-        if (OVRInput.GetDown(OVRInput.RawTouch.B))
+        else 
         {
-            Debug.Log("Bボタンを押した");
-        }
-        if (OVRInput.GetDown(OVRInput.RawButton.X))
-        {
-            Debug.Log("Xボタンを押した");
-        }
-        if (OVRInput.GetDown(OVRInput.RawButton.Y))
-        {
-            Debug.Log("Yボタンを押した");
-        }
-        if (OVRInput.GetDown(OVRInput.RawButton.Start))
-        {
-            Debug.Log("メニューボタン（左アナログスティックの下にある）を押した");
+            Debug.Log("ssssssssssssssssssssssss");
         }
 
-        if (OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger))
-        {
-            Debug.Log("右人差し指トリガーを押した");
-        }
-        if (OVRInput.GetDown(OVRInput.RawButton.RHandTrigger))
-        {
-            Debug.Log("右中指トリガーを押した");
-        }
-        if (OVRInput.GetDown(OVRInput.RawButton.LIndexTrigger))
-        {
-            Debug.Log("左人差し指トリガーを押した");
-        }
-        if (OVRInput.GetDown(OVRInput.RawButton.LHandTrigger))
-        {
-            Debug.Log("左中指トリガーを押した");
-        }
+        //if (OVRInput.GetDown(OVRInput.Touch.One))
+        //{
+        //    Debug.Log("Aボタンを押した");
+        //}
+        //if (OVRInput.GetDown(OVRInput.RawTouch.B))
+        //{
+        //    Debug.Log("Bボタンを押した");
+        //}
+        //if (OVRInput.GetDown(OVRInput.RawButton.X))
+        //{
+        //    Debug.Log("Xボタンを押した");
+        //}
+        //if (OVRInput.GetDown(OVRInput.RawButton.Y))
+        //{
+        //    Debug.Log("Yボタンを押した");
+        //}
+        //if (OVRInput.GetDown(OVRInput.RawButton.Start))
+        //{
+        //    Debug.Log("メニューボタン（左アナログスティックの下にある）を押した");
+        //}
+
+        //if (OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger))
+        //{
+        //    Debug.Log("右人差し指トリガーを押した");
+        //}
+        //if (OVRInput.GetDown(OVRInput.RawButton.RHandTrigger))
+        //{
+        //    Debug.Log("右中指トリガーを押した");
+        //}
+        //if (OVRInput.GetDown(OVRInput.RawButton.LIndexTrigger))
+        //{
+        //    Debug.Log("左人差し指トリガーを押した");
+        //}
+        //if (OVRInput.GetDown(OVRInput.RawButton.LHandTrigger))
+        //{
+        //    Debug.Log("左中指トリガーを押した");
+        //}
     }
 }
