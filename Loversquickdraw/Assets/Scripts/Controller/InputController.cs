39245f1b06471878695cde07ee95831070498f06
kazuya <yuuki0523yata@gmail.com>
2019-05-19 10:22:45 +0900

    コントローラーのPosを簡単にテキストに表示 明日そこから振るところを取得する

--- Loversquickdraw/Assets/Scripts/Controller/InputController.cs
+++ Loversquickdraw/Assets/Scripts/Controller/InputController.cs
@@ -1,28 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 //岩崎
 public class InputController : MonoBehaviour
 {
     [SerializeField] GameObject Lcube;
     [SerializeField] GameObject Rcube;
+    [SerializeField] Text L_text;
+    [SerializeField] Text R_text;
     int a = 0;
     Vector3 L_defPos;
     Vector3 R_defPos;
+    
     void Update ()
     {
 
         L_defPos = Lcube.gameObject.transform.position;
         R_defPos = Rcube.gameObject.transform.position;
-        if (L_defPos.y < -0.7f || L_defPos.y > 0f)
-        {
-            Debug.Log("aaa");
-        }
-        else 
-        {
-            Debug.Log("ssssssssssssssssssssssss");
-        }
+        L_text.text = L_defPos.ToString();
+        R_text.text = R_defPos.ToString();
+        //if (L_defPos.y < -0.7f || L_defPos.y > 0f)
+        //{
+        //    Debug.Log("aaa");
+        //}
+        //else 
+        //{
+        //    Debug.Log("ssssssssssssssssssssssss");
+        //}
 
         //if (OVRInput.GetDown(OVRInput.Touch.One))
         //{
