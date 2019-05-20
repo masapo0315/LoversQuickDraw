9c9a70110c39ef97424e91c2baec252aabb149cd
kazuya <yuuki0523yata@gmail.com>
2019-05-20 15:32:19 +0900

    あと2割くらい

--- Loversquickdraw/Assets/Scripts/Controller/InputController.cs
+++ Loversquickdraw/Assets/Scripts/Controller/InputController.cs
@@ -6,80 +6,54 @@ using UnityEngine.UI;
 //岩崎
 public class InputController : MonoBehaviour
 {
-    [SerializeField] GameObject Lcube;
-    [SerializeField] GameObject Rcube;
-    [SerializeField] Text L_text;
-    [SerializeField] Text R_text;
+    [SerializeField] private GameObject Lcube;
+    [SerializeField] private GameObject Rcube;
+    [SerializeField] private Text L_text;
+    [SerializeField] private Text R_text;
 
-    Vector3 L_defPos;
-    Vector3 R_defPos;
-    
+    private Vector3 L_defPos;
+    private Vector3 R_defPos;
 
+    private int L_count = 0;
+    private int R_count = 0;
+
+    private bool L_Concheck = false;
+    private bool R_Concheck = false;
 
     private void Start()
     {
         L_defPos = Lcube.transform.position;
         R_defPos = Rcube.transform.position;
     }
+
     void Update ()
     {
-        Debug.Log(L_defPos.y);
-        Debug.Log(R_defPos.y);
-        if (Lcube.transform.position.y >= L_defPos.y + 0.3f && Lcube.transform.position.y <= L_defPos.y - 0.3f)
-        {
-            Debug.Log("L_Conふってる");
-        }
-        else if (Lcube.transform.position.y < L_defPos.y + 0.3f && Lcube.transform.position.y > L_defPos.y - 0.3f)
-        {
-            Debug.Log("L_Conふってる");
-        }
         L_text.text = Lcube.transform.position.y.ToString();
         R_text.text = Rcube.transform.position.y.ToString();
-        //if (L_defPos.y < -0.7f || L_defPos.y > 0f)
-        //{
-        //    Debug.Log("aaa");
-        //}
-        //else 
-        //{
-        //    Debug.Log("ssssssssssssssssssssssss");
-        //}
+        //Debug.Log(L_defPos.y);
+        //Debug.Log(R_defPos.y);
+        if (L_Concheck == false && Lcube.transform.position.y < L_defPos.y + 0.02f && Lcube.transform.position.y > L_defPos.y - 0.02f)
+        {
+            L_Concheck = true;
+        }
+        if (L_Concheck == true)
+        {
+            L_count = L_count + 1;
+            Debug.Log(L_count);
+            Debug.Log("L_Con反応アリ");
+            L_Concheck = false;
+        }
 
-        //if (OVRInput.GetDown(OVRInput.Touch.One))
-        //{
-        //    Debug.Log("Aボタンを押した");
-        //}
-        //if (OVRInput.GetDown(OVRInput.RawTouch.B))
-        //{
-        //    Debug.Log("Bボタンを押した");
-        //}
-        //if (OVRInput.GetDown(OVRInput.RawButton.X))
-        //{
-        //    Debug.Log("Xボタンを押した");
-        //}
-        //if (OVRInput.GetDown(OVRInput.RawButton.Y))
-        //{
-        //    Debug.Log("Yボタンを押した");
-        //}
-        //if (OVRInput.GetDown(OVRInput.RawButton.Start))
-        //{
-        //    Debug.Log("メニューボタン（左アナログスティックの下にある）を押した");
-        //}
-
-        //if (OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger))
-        //{
-        //    Debug.Log("右人差し指トリガーを押した");
-        //}
-        //if (OVRInput.GetDown(OVRInput.RawButton.RHandTrigger))
-        //{
-        //    Debug.Log("右中指トリガーを押した");
-        //}
-        //if (OVRInput.GetDown(OVRInput.RawButton.LIndexTrigger))
-        //{
-        //    Debug.Log("左人差し指トリガーを押した");
-        //}
-        //if (OVRInput.GetDown(OVRInput.RawButton.LHandTrigger))
-        //{
-        //    Debug.Log("左中指トリガーを押した");
-        //}
+        if (R_Concheck == false && Rcube.transform.position.y < R_defPos.y + 0.02f && Rcube.transform.position.y > R_defPos.y - 0.02f)
+        {
+            R_Concheck = true;
+        }
+        if(R_Concheck == true)
+        {
+            R_Concheck = false;
+            R_count = R_count + 1;
+            Debug.Log(L_count);
+            Debug.Log("R_Con反応アリ");
+        }
     }
 }
