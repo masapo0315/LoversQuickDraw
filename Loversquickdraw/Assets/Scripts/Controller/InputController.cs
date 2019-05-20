a9b2ebccd7f2960f4290ff2ccc8d9872039017d2
kazuya <yuuki0523yata@gmail.com>
2019-05-20 12:02:43 +0900

    作業少し進展

--- Loversquickdraw/Assets/Scripts/Controller/InputController.cs
+++ Loversquickdraw/Assets/Scripts/Controller/InputController.cs
@@ -10,17 +10,31 @@ public class InputController : MonoBehaviour
     [SerializeField] GameObject Rcube;
     [SerializeField] Text L_text;
     [SerializeField] Text R_text;
-    int a = 0;
+
     Vector3 L_defPos;
     Vector3 R_defPos;
     
+
+
+    private void Start()
+    {
+        L_defPos = Lcube.transform.position;
+        R_defPos = Rcube.transform.position;
+    }
     void Update ()
     {
-
-        L_defPos = Lcube.gameObject.transform.position;
-        R_defPos = Rcube.gameObject.transform.position;
-        L_text.text = L_defPos.ToString();
-        R_text.text = R_defPos.ToString();
+        Debug.Log(L_defPos.y);
+        Debug.Log(R_defPos.y);
+        if (Lcube.transform.position.y >= L_defPos.y + 0.3f && Lcube.transform.position.y <= L_defPos.y - 0.3f)
+        {
+            Debug.Log("L_Conふってる");
+        }
+        else if (Lcube.transform.position.y < L_defPos.y + 0.3f && Lcube.transform.position.y > L_defPos.y - 0.3f)
+        {
+            Debug.Log("L_Conふってる");
+        }
+        L_text.text = Lcube.transform.position.y.ToString();
+        R_text.text = Rcube.transform.position.y.ToString();
         //if (L_defPos.y < -0.7f || L_defPos.y > 0f)
         //{
         //    Debug.Log("aaa");
