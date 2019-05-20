8d1a96d1cb337ba17a06c2f32fca1ee7ca04db4b
Tomioka <17c1020019tt@edu.tech.ac.jp>
2019-05-20 19:30:08 +0900

    選択肢を選ぶと、選んでない選択肢の色を暗くし他の入力を断つ(二人目のプレイヤーは考慮してない)

--- Loversquickdraw/Assets/Scripts/ChoiceManager.cs
+++ Loversquickdraw/Assets/Scripts/ChoiceManager.cs
@@ -3,22 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+//富岡
 public class ChoiceManager : MonoBehaviour
 {
+    /// <summary>
+    /// 選択肢を選んだあとその選択肢を光らせ
+    /// 選ばなかった選択肢を暗くし
+    /// 一定時間後に選択肢を消す
+    /// </summary>
 
     [SerializeField]
     GameObject Choice1;
     [SerializeField]
+    GameObject Choice1Text;
+
+    [SerializeField]
     GameObject Choice2;
     [SerializeField]
-    GameObject Choice3;
+    GameObject Choice2Text;
 
+    [SerializeField]
+    GameObject Choice3;
+    [SerializeField]
+    GameObject Choice3Text;
 
     bool StopChoice = false;
-
-    float a = 0;
-
-    int hoge = 0;
 
     // Use this for initialization
     void Start()
@@ -29,59 +38,75 @@ public class ChoiceManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //ボタンを押し選択肢を選んだら、それ以降ボタンを消し入力を断つ
         if (Input.GetKeyDown(KeyCode.Keypad1) && StopChoice == false)
         {
             Debug.Log("1を押した");
-            hoge = 1;
-            Choise1();
+            ChangeColor1();
+            Invoke("Choise1", 3.5f);
             StopChoice = true;
-
         }
 
         if (Input.GetKeyDown(KeyCode.Keypad2) && StopChoice == false)
         {
             Debug.Log("2を押した");
-            hoge = 2;
-            Choise2();
+            ChangeColor2();
+            Invoke("Choise2", 3.5f);
             StopChoice = true;
-
         }
 
         if (Input.GetKeyDown(KeyCode.Keypad3) && StopChoice == false)
         {
             Debug.Log("3を押した");
-            hoge = 3;
-            Choise3();
+            ChangeColor3();
+            Invoke("Choise3", 3.5f);
             StopChoice = true;
         }
+
+    }
+    //カラーコードは〇〇/255, で表示
+    private void ChangeColor1()
+    {
+        Debug.Log("2と3を暗くする");
+        Choice2.GetComponent<Image>().color = new Color(120 / 255f, 120 / 255f, 120 / 255f);
+        Choice3.GetComponent<Image>().color = new Color(120 / 255f, 120 / 255f, 120 / 255f);
     }
 
-    void Choise1()
+    private void ChangeColor2()
+    {
+        Debug.Log("1と3を暗くする");
+        Choice1.GetComponent<Image>().color = new Color(120 / 255f, 120 / 255f, 120 / 255f);
+        Choice3.GetComponent<Image>().color = new Color(120 / 255f, 120 / 255f, 120 / 255f);
+    }
+
+    private void ChangeColor3()
+    {
+        Debug.Log("1と2を暗くする");
+        Choice1.GetComponent<Image>().color = new Color(120 / 255f, 120 / 255f, 120 / 255f);
+        Choice2.GetComponent<Image>().color = new Color(120 / 255f, 120 / 255f, 120 / 255f);
+    }
+
+
+
+    private void Choise1()
     {
         Choice2.SetActive(false);
         Choice3.SetActive(false);
-        /*
-        switch (hoge)
-        {
-           case :
-                Choice2.SetActive(false);
-                Choice3.SetActive(false);
-                break;
-        }
-
-    */
+        Debug.Log("Choise1を通った");      
     }
 
-    void Choise2()
+    private void Choise2()
     {
         Choice1.SetActive(false);
         Choice3.SetActive(false);
+        Debug.Log("Choise2を通った");
     }
 
-    void Choise3()
+    private void Choise3()
     {
         Choice1.SetActive(false);
         Choice2.SetActive(false);
+        Debug.Log("Choise3を通った");
     }
 
 }
