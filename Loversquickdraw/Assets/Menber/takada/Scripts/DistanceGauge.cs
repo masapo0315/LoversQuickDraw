6ecec932c2946b6fea6b6649ac94570d8d5f1069
高田将志 <masapo.0315@icloud.com>
2019-05-24 17:18:05 +0900

    Scriptをいじりました。

--- Loversquickdraw/Assets/Menber/takada/Scripts/DistanceGauge.cs
+++ Loversquickdraw/Assets/Menber/takada/Scripts/DistanceGauge.cs
@@ -5,15 +5,28 @@ using UnityEngine.UI;
 
 public class DistanceGauge : MonoBehaviour {
 
+    //playerのゴールまでの距離に連動してゲージを変化させる
+
     Slider distanceSlider;
+    public GameObject player;
 
 	// Use this for initialization
 	void Start () {
         distanceSlider = GetComponent<Slider>();
+        player = GetComponent<GameObject>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        ValuePlus();
 	}
+    
+    //playerのz地点でvalueが変化
+    void ValuePlus()
+    {
+        Vector3 pos = player.transform.position;
+        float z = pos.z;
+
+        distanceSlider.value = z;
+    }
 }
