e163426951a6c67f1bbfc4adb6bee09c2d6cf95e
高田将志 <masapo.0315@icloud.com>
2019-06-20 23:55:08 +0900

    キーボード操作で再びプレイヤーを動かせるようにした。

--- Loversquickdraw/Assets/Menber/takada/Scripts/TestPlayer2Controler.cs
+++ Loversquickdraw/Assets/Menber/takada/Scripts/TestPlayer2Controler.cs
@@ -2,29 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestPlayerControler1 : MonoBehaviour {
+public class TestPlayer2Controler : MonoBehaviour {
 
     public Rigidbody rb;
     float movespeed = 5.0f;
     float moveForceMultipliter = 1.0f;
 
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
 
         rb = GetComponent<Rigidbody>();
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         SpeedUp();
-	}
+    }
 
     void SpeedUp()
     {
         Vector3 moveVector = Vector3.zero;
-        float horizontalInput = Input.GetAxis("Horizontal");
+        float horizontalInput = Input.GetAxis("Horizontal2");
         moveVector.x = movespeed * horizontalInput;
 
         rb.AddForce(moveForceMultipliter * (moveVector - rb.velocity));
