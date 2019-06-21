207eeb6188a43d85a174a6a3cfefc1cbd224d5dd
高田将志 <masapo.0315p@icloud.com>
2019-06-21 18:52:13 +0900

    障害物のランダム生成のスクリプトを作成途中

---
+++ Loversquickdraw/Assets/Menber/takada/Scripts/Obstacle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour {
+
+    [SerializeField] GameObject obstacle;
+    [SerializeField] GameObject Player1;
+    [SerializeField] GameObject obstacle2;
+    [SerializeField] GameObject Player2;
+
+    // Update is called once per frame
+    void Update () {
+        ObstacleSporn();
+	}
+
+    void ObstacleSporn()
+    {
+        Instantiate(obstacle, new Vector3(Player1.transform.position.x + 5f,-50,0), Quaternion.identity);
+        Instantiate(obstacle, new Vector3(Player2.transform.position.x + 5f, -100, 0), Quaternion.identity);
+    }
+
+}
