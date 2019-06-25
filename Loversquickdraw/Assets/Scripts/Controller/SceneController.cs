73b728b7f533059e07f833c6fdaca63307be594c
kazuya <yuuki0523yata@gmail.com>
2019-06-25 17:33:36 +0900

    ボタンが反応するようになった。 ほかのsceneに同じ作業で使えるようにする(これから)

--- Loversquickdraw/Assets/Scripts/Controller/SceneController.cs
+++ Loversquickdraw/Assets/Scripts/Controller/SceneController.cs
@@ -6,36 +6,19 @@ using UnityEngine.SceneManagement;
 //岩崎
 public class SceneController : MonoBehaviour
 {
-    //inputManagerからの取得を
-    private OVRInput.Button _button;
-    private int _buttonNum = 1;
-    private void Start()
-    {
-        _button = InputManager.GetButton(_buttonNum);
-        //Debug.Log(_button);
-    }
     void Update ()
     {
-        //Debug.Log(_button);
-        //DebugInput();
-        CheckInput(_button);
         if(OVRInput.GetDown(OVRInput.Button.One))
         {
-            Debug.Log(_button);
+            SceneManager.LoadScene("Scenario");
+            Debug.Log("Aボタン");
         }
+       // DebugInput();
     }
     private void DebugInput()
     {
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            SceneManager.LoadScene("Scenario");
-        }
-    }
-    private void CheckInput(OVRInput.Button button)
-    {
-        if(/*SceneManager.GetActiveScene().name == "Title" &&*/ OVRInput.GetDown(button))
-        {
-            Debug.Log(button);
             SceneManager.LoadScene("Scenario");
         }
     }
