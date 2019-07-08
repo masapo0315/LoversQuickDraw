5f4dec71f9c83505096f545dd330422ca788fcc0
kazuya <yuuki0523yata@gmail.com>
2019-07-08 14:59:50 +0900

    コントローラーのスティックの取得おわり

--- Loversquickdraw/Assets/Scripts/Other/Test.cs
+++ Loversquickdraw/Assets/Scripts/Other/Test.cs
@@ -6,6 +6,7 @@ public class Test : MonoBehaviour
 {
     void Update()
     {
+        //ボタン系
         if (OVRInput.GetDown(OVRInput.RawButton.A))
         {
             Debug.Log("Aボタンを押した");
@@ -26,6 +27,7 @@ public class Test : MonoBehaviour
         {
             Debug.Log("メニューボタン（左アナログスティックの下にある）を押した");
         }
+        //右コントリガー系
         if (OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger))
         {
             Debug.Log("右人差し指トリガーを押した");
@@ -34,6 +36,7 @@ public class Test : MonoBehaviour
         {
             Debug.Log("右中指トリガーを押した");
         }
+        //左コントリガー系
         if (OVRInput.GetDown(OVRInput.RawButton.LIndexTrigger))
         {
             Debug.Log("左人差し指トリガーを押した");
@@ -41,6 +44,40 @@ public class Test : MonoBehaviour
         if (OVRInput.GetDown(OVRInput.RawButton.LHandTrigger))
         {
             Debug.Log("左中指トリガーを押した");
+        }
+        //左スティック系
+        if (OVRInput.GetDown(OVRInput.RawButton.LThumbstickUp))
+        {
+            Debug.Log("左アナログスティックを上に傾けた");
+        }
+        if (OVRInput.GetDown(OVRInput.RawButton.LThumbstickDown))
+        {
+            Debug.Log("左アナログスティックを下に傾けた");
+        }
+        if (OVRInput.GetDown(OVRInput.RawButton.LThumbstickLeft))
+        {
+            Debug.Log("左アナログスティックを左に傾けた");
+        }
+        if (OVRInput.GetDown(OVRInput.RawButton.LThumbstickRight))
+        {
+            Debug.Log("左アナログスティックを右に傾けた");
+        }
+        //右スティック系
+        if(OVRInput.GetDown(OVRInput.RawButton.RThumbstickUp))
+        {
+            Debug.Log("右アナログスティックを上に傾けた");
+        }
+        if (OVRInput.GetDown(OVRInput.RawButton.RThumbstickDown))
+        {
+            Debug.Log("右アナログスティックを下に傾けた");
+        }
+        if (OVRInput.GetDown(OVRInput.RawButton.RThumbstickLeft))
+        {
+            Debug.Log("右アナログスティックを左に傾けた");
+        }
+        if (OVRInput.GetDown(OVRInput.RawButton.RThumbstickRight))
+        {
+            Debug.Log("右アナログスティックを右に傾けた");
         }
     }
 }
