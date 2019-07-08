a2526810284eb22460b39a71ceda90fe3175df58
高田将志 <masapo.0315p@icloud.com>
2019-07-08 19:28:37 +0900

    シングルトンの作りかけ

---
+++ Loversquickdraw/Assets/Scripts/Other/Singlton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singlton : MonoBehaviour {
+
+    private static Singlton instance = null;
+
+    public static Singlton Instance
+    {
+        get
+        {
+            if (instance == null) instance = new Singlton();
+
+            return instance;
+        }
+    }
+
+    public static int[] WinFlag = {0, 0,};
+
+}
