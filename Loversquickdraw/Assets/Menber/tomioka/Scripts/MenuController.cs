f557f00f9551d819eb3e4b5c7b89716799df7ee3
Tomioka <17c1020019tt@edu.tech.ac.jp>
2019-07-15 17:15:12 +0900

    メニュー作る

---
+++ Loversquickdraw/Assets/Menber/tomioka/Scripts/MenuController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject MenuWindow;
+
+    public void MenuOn()
+    {
+        MenuWindow.SetActive(true);
+    }
+
+    public void MenuOff()
+    {
+        MenuWindow.SetActive(false);
+    }
+
+}
