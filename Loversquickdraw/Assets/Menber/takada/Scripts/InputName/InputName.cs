d55747905102b687d7cd1ac7116c52c4387789e3
高田将志 <masapo.0315p@icloud.com>
2019-08-26 19:55:46 +0900

    入力した文字を削除できるようにした

--- Loversquickdraw/Assets/Menber/takada/Scripts/InputName/InputName.cs
+++ Loversquickdraw/Assets/Menber/takada/Scripts/InputName/InputName.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class InputName : MonoBehaviour {
 
@@ -10,6 +11,7 @@ public class InputName : MonoBehaviour {
     [SerializeField]
     private Text inputname = null;
 
+    //文字数
     private int nameCount;
 
     public static string Player1Name;
@@ -23,18 +25,23 @@ public class InputName : MonoBehaviour {
     //テキストの中に文字を追加
     public void OnAddString(string inputString)
     {
+        //１０文字以上入力できないようにする
         if(nameCount < 10)
         {
             inputname.text += inputString;
             nameCount += 1;
-
         }
-    } 
+    }
 
     //テキストの中の文字を削除
     public void DestroyString()
     {
+        //文字が入っていなければ何もしない
+        if (nameCount == 0) return;
 
+        string text = inputname.text.Substring(0,inputname.text.Length-1);
+        inputname.text = text;
+        nameCount -= 1;
     }
 
     //名前の決定
