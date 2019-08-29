c24dbaee90ae4bd9b4369415c17d2b1b46b8fa95
高田将志 <masapo.0315@icloud.com>
2019-08-30 01:09:07 +0900

    カーソルを合わせたときに1文字ずつではないが文字の入力はできるようになった

--- Loversquickdraw/Assets/Menber/takada/Scripts/InputName/InputName.cs
+++ Loversquickdraw/Assets/Menber/takada/Scripts/InputName/InputName.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System;
+using UnityEngine.EventSystems;
 
 public class InputName : MonoBehaviour {
 
     //名前入力に関係するScript
+    
 
     [SerializeField]
     private Text inputname = null;
@@ -23,55 +25,65 @@ public class InputName : MonoBehaviour {
     public static string Player1Name;
 
     [SerializeField]
-    private GameObject Input;
+    private GameObject input;
 
     [SerializeField]
     private GameObject Confirmation;
 
+    private void Start()
+    {
+
+    }
+
     //テキストの中に文字を追加
     public void OnAddString(string inputString)
     {
-        //１０文字以上入力できないようにする
-        if(nameCount < 10)
-        {
-            inputname.text += inputString;
-            nameCount += 1;
-            nomalText.gameObject.SetActive(true);
-            errorText.gameObject.SetActive(false);
-        }
 
+        //１０文字以上入力できないようにする
+        if (nameCount < 10){
+
+             inputname.text += inputString;
+             nameCount += 1;
+            Debug.Log(nameCount);
+             nomalText.gameObject.SetActive(true);
+             errorText.gameObject.SetActive(false);
+
+        }
+        
     }
 
     //テキストの中の文字を削除
     public void DestroyString()
     {
         //文字が入っていなければ何もしない
-        if (nameCount == 0) return;
+        if (nameCount == 0) { return; }  
 
-        string text = inputname.text.Substring(0,inputname.text.Length-1);
+        string text = inputname.text.Substring(0, inputname.text.Length - 1);
         inputname.text = text;
         nameCount -= 1;
+        
     }
 
+    
     //名前の決定
     public void EnterName()
     {
         //名前が入力されていなかったら決定できないようにする
-        if(nameCount != 0)
+        if (nameCount != 0)
         {
-            Player1Name = inputname.text;
+             Player1Name = inputname.text;
 
-            Input.SetActive(false);
-            Confirmation.SetActive(true);
-
+             input.SetActive(false);
+             Confirmation.SetActive(true);
 
         } else {
 
-            nomalText.gameObject.SetActive(false);
-            errorText.gameObject.SetActive(true);
+             nomalText.gameObject.SetActive(false);
+             errorText.gameObject.SetActive(true);
 
-        }
-
+         }
+        
     }
+    
 
 }
