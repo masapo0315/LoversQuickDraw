2a770aa90fc1e09b2ab0d1335f458ed2472d56ec
高田将志 <masapo.0315p@icloud.com>
2019-08-30 11:01:34 +0900

    VR用のスクリプトを作成

---
+++ Loversquickdraw/Assets/Menber/takada/Scripts/InputName/VR/DestryNameVR.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using UnityEngine.EventSystems;
+
+public class DestryNameVR : MonoBehaviour {
+
+    private InputNameVR inputNameVR;
+
+    [SerializeField]
+    private Text inputname;
+
+    int namecount;
+
+	// Use this for initialization
+	void Start () {
+
+        inputname = inputNameVR.inputname;
+        namecount = inputNameVR.nameCount;
+
+	}
+
+    private void OnTriggerEnter(Collider col)
+    {
+        DestroyString(col);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        DestroyString(other);
+    }
+
+    //テキストの中の文字を削除
+    public void DestroyString(Collider _col)
+    {
+        if (_col.gameObject.tag == "Icon" && Input.GetButton("submit"))
+        {
+            //文字が入っていなければ何もしない
+            if (namecount == 0) { return; }
+
+            string text = inputname.text.Substring(0, inputname.text.Length - 1);
+            inputname.text = text;
+            namecount -= 1;
+
+        }
+    }
+}
---
+++ Loversquickdraw/Assets/Menber/takada/Scripts/InputName/VR/EnterNameVR.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using UnityEngine.EventSystems;
+
+public class EnterNameVR : MonoBehaviour {
+
+    private InputNameVR inputNameVR;
+
+    [SerializeField]
+    private Text inputname;
+
+    int namecount;
+
+    // Use this for initialization
+    void Start () {
+
+        inputname = inputNameVR.inputname;
+        namecount = inputNameVR.nameCount;
+
+    }
+
+
+
+    //名前の決定
+    public void EnterName()
+    {
+        //名前が入力されていなかったら決定できないようにする
+        if (namecount != 0)
+        {
+            Player1name = inputname.text;
+
+            input.SetActive(false);
+            Confirmation.SetActive(true);
+
+        }
+        else
+        {
+
+            nomalText.gameObject.SetActive(false);
+            errorText.gameObject.SetActive(true);
+
+        }
+
+    }
+
+}
