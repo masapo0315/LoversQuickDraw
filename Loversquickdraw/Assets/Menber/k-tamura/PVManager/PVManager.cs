7fee00fff622c6a92426d005ae6d11c029bfd507
k-tamura <kuro@kuro.games>
2019-09-01 16:36:16 +0900

    一定時間がたったらPVを再生するように変更

---
+++ Loversquickdraw/Assets/Menber/k-tamura/PVManager/PVManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class PVManager : MonoBehaviour
+{
+    [Header("VideoPlayer")]
+    [SerializeField]VideoClip VideoClip;
+    [SerializeField]VideoPlayer VideoPlayer;
+    [SerializeField]float Volume=70;
+    [Header("Log")]
+    [SerializeField] bool Sceneload;
+    [SerializeField] float nowVolume;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        VideoPlayer.SetDirectAudioVolume(0,Volume / 100);
+        VideoPlayer.Play();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Sceneload)
+        {
+            if (VideoPlayer.time >= (int)VideoPlayer.clip.length || Input.anyKey)
+            {
+                SceneLoadManager.LoadScene("Title");
+                Sceneload = true;
+            }
+        }
+        else
+        {
+            while (true)
+            {
+                if (VideoPlayer.GetDirectAudioVolume(0) <= 0) break;
+                nowVolume = VideoPlayer.GetDirectAudioVolume(0) -0.00001f;
+                VideoPlayer.SetDirectAudioVolume(0,nowVolume);
+            }
+        }
+    }
+}
--- Loversquickdraw/Assets/Scripts/Controller/SceneController.cs
+++ Loversquickdraw/Assets/Scripts/Controller/SceneController.cs
@@ -5,18 +5,27 @@ using UnityEngine;
 public class SceneController : MonoBehaviour
 {
     private bool once;
+    [SerializeField] float waittime;
+    [SerializeField] float time;
     private void Start()
     {
         Cursor.visible = false;
+        once = false;
     }
     void Update ()
     {
+        time += Time.deltaTime;
         if (once == false)
         {
             if (OVRInput.GetDown(OVRInput.Button.One) || Input.GetKeyDown(KeyCode.Space))
             {
                 once = true;
                 SceneLoadManager.LoadScene("Scenario");
+            }
+            else if(time>=waittime)
+            {
+                once = true;
+                SceneLoadManager.LoadScene("PVScene");
             }
         }
 	}
