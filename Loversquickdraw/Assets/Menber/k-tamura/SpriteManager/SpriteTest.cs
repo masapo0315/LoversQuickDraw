5bfb78e4bff692e759d90e55580beff5e8b04383
k.tamura <kuro@kuro.games>
2019-09-06 11:25:16 +0900

    SpriteManagerほぼ完成

---
+++ Loversquickdraw/Assets/Menber/k-tamura/SpriteManager/SpriteTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteTest : MonoBehaviour {
+
+	public void test()
+    {
+        SpriteManager.SpriteSwitch(0);
+    }
+    public void　test2()
+    {
+        SpriteManager.SpriteSwitch(1);
+
+    }
+    public void test3()
+    {
+        SpriteManager.SpriteSwitch(-1);
+    }
+}
