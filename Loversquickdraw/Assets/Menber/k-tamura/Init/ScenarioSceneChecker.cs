be28cce821066d7a4ba752142e6d8006d8f9f55d
98kuro <kuro@kuro.games>
2019-09-09 01:19:11 +0900

    ScenarioSceneCheckerほぼ完成

---
+++ Loversquickdraw/Assets/Menber/k-tamura/Init/ScenarioSceneChecker.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScenarioSceneChecker : MonoBehaviour {
+
+    TalkManager talkManager;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (talkManager._isSeEnd)
+        {
+            int i = int.Parse(PlayerPrefs.GetString("ScenarioNum"));
+            switch (i)
+            {
+                case 0:
+                    SceneLoadManager.LoadScene("");
+                    break;
+                case 1:
+                    SceneLoadManager.LoadScene("");
+                    break;
+                default:
+                    SceneLoadManager.LoadScene("Init");
+                    break;
+            }
+            PlayerPrefs.SetString("ScenarioNum", i + 1.ToString());
+        }
+		
+	}
+}
