dc0643c6351cc22ee9101a65f0d5afd307d6a00c
98kuro <kuro@kuro.games>
2019-09-10 04:20:31 +0900

    告白４コントローラーのバイブレーション

---
+++ Loversquickdraw/Assets/Menber/k-tamura/Coninit/OVRControllerVib/OVRControllerVib.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OVRControllerVib : SingletonMonoBehaviour<OVRControllerVib> {
+    OVRHapticsClip hapticsClip;
+    // Use this for initialization
+    void Start () {
+        byte[] samples = new byte[1000];
+        for (int i = 0; i < samples.Length; i++)
+        {
+            samples[i] = 128;
+        }
+        hapticsClip = new OVRHapticsClip(samples, samples.Length);
+    }
+	
+	// Update is called once per frame
+	public static void ControllerVib(int i)
+    {
+        if (i == 0)
+        {
+            OVRHaptics.LeftChannel.Mix(Instance.hapticsClip);
+        }
+        else if(i==1)
+        {
+            OVRHaptics.RightChannel.Mix(Instance.hapticsClip);
+        }
+    }
+}
