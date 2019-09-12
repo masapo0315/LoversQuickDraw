be778631e747770a676730b77e2b652393708cef
98kuro <kuro@kuro.games>
2019-09-12 21:47:45 +0900

    F2で強制終了、F4で初期化機能実装

---
+++ Loversquickdraw/Assets/Menber/k-tamura/ESCQuit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ESCQuit : SingletonMonoBehaviour<ESCQuit> {
+
+	// Use this for initialization
+	void Start () {
+        DontDestroyOnLoad(this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.F2))
+            Application.Quit();
+        if (Input.GetKeyDown(KeyCode.F4))
+        {
+            PlayerPrefs.SetString("ScenarioNum", "0");
+            UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+        }
+    }
+}
